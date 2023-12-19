fa4b5a8214c113901482cbf31695e18ef5504702
Serhat <serhat16_@hotmail.com>
2023-12-19 09:50:41 +0300

    Add training and social interaction migration.

---
+++ VirtualPetCare/VirtualPetCare.Core/Entities/SocialInteraction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetCare.Core.Entities
+{
+    public class SocialInteraction : BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public List<Pet> Pets { get; set; }
+    }
+}
